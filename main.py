1eea2fc751a1ac186bac5cfa5def89f98567b7dc
cassandra-garzia <58474719+cassandra-garzia@users.noreply.github.com>
2023-04-10 20:50:32 -0400

    Updated Needs

--- main.py
+++ main.py
@@ -7,5 +7,18 @@ def index():
     data = "Hello from RAT!"
     return render_template("index.html", data = data)
 
+@app.route("/upload", methods = ["POST"])
+def upload_image():
+    # Define Image File
+    return
+def upload_mask():
+    # Define Mask File
+    return
+
+@app.route("/export")
+def export_file():
+    # Define exported file
+    return
+
 if __name__ == "__main__":
     app.run()
